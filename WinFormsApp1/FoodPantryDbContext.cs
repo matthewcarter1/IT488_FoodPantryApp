110d4a21276f919fd5bdeb6733dd91b3bac95174
matthewcarter1 <matthewcarter@student.purdueglobal.edu>
2023-02-10 14:03:52 -0500

    Added comments

--- WinFormsApp1/FoodPantryDbContext.cs
+++ WinFormsApp1/FoodPantryDbContext.cs
@@ -5,48 +5,62 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
+
 namespace WinFormsApp1
 {
+    // Class that defines the FoodPantryDB database context
     public class FoodPantryDbContext : DbContext
     {
+        // Override method that sets up the connection to the database
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseSqlServer("Data Source=DESKTOP-IU3R9KA\\SQLEXPRESS;Initial Catalog=FoodPantryDB;Integrated Security=False;TrustServerCertificate=True;");
+            // Set up the connection string using SQL Server
             optionsBuilder.UseSqlServer("Data Source=DESKTOP-IU3R9KA\\SQLEXPRESS;Initial Catalog=FoodPantryDB;User Id=FoodPantry;Password=PantryFood;TrustServerCertificate=True;");
-
         }
+
+        // DbSet properties that represent the tables in the database
         public DbSet<Item> Items { get; set; }
         public DbSet<Category> Categories { get; set; }
     }
-}
-public class Item
-{
-    public int ItemID { get; set; }
-    public string ItemName { get; set; }
-    public string ItemDescription { get; set; }
-    public int CategoryID { get; set; }
-    public int Quantity { get; set; }
-    public string Unit { get; set; }
 
-    public Category Category { get; set; }
-
-    public Item()
+    // Class that represents the Item table in the database
+    public class Item
     {
-        ItemName = "";
-        ItemDescription = "";
-        Unit = "";
-        Category = new Category();
+        // Properties of the Item class
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public string ItemDescription { get; set; }
+        public int CategoryID { get; set; }
+        public int Quantity { get; set; }
+        public string Unit { get; set; }
+
+        // Navigation property for the relationship with the Category table
+        public Category Category { get; set; }
+
+        // Default constructor
+        public Item()
+        {
+            ItemName = "";
+            ItemDescription = "";
+            Unit = "";
+            Category = new Category();
+        }
     }
-}
 
-public class Category
-{
-    public int CategoryID { get; set; }
-    public string CategoryName { get; set; }
-
-    public ICollection<Item> Items { get; set; }
-    public Category()
+    // Class that represents the Category table in the database
+    public class Category
     {
-        CategoryName = string.Empty;
+        // Properties of the Category class
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+        // Navigation property for the relationship with the Item table
+        public ICollection<Item> Items { get; set; }
+
+        // Default constructor
+        public Category()
+        {
+            CategoryName = string.Empty;
+        }
     }
 }
--- WinFormsApp1/ItemService.cs
+++ WinFormsApp1/ItemService.cs
@@ -8,32 +8,43 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WinFormsApp1
 {
+    // Class definition for the ItemService
     public class ItemService
     {
+        // Private field to store the database context
         private readonly FoodPantryDbContext _dbContext;
 
+        // Constructor that takes in the database context and stores it in the private field
         public ItemService(FoodPantryDbContext dbContext)
         {
             _dbContext = dbContext;
         }
 
+        // Method to add a new item to the database
         public void AddItem(Item item)
         {
+            // Check if the item already exists in the database
             var existingItem = _dbContext.Items.FirstOrDefault(i => i.ItemName == item.ItemName);
+
+            // If the item already exists, update the quantity
             if (existingItem != null)
             {
                 existingItem.Quantity += item.Quantity;
             }
+            // If the item does not exist, add it to the database
             else
             {
                 _dbContext.Items.Add(item);
             }
 
+            // Save the changes to the database
             _dbContext.SaveChanges();
         }
 
+        // Method to retrieve all items from the database
         public IEnumerable<Item> GetItems()
         {
+            // Return a list of items including the category information
             return _dbContext.Items
                 .Include(i => i.Category)
                 .Select(i => new Item
@@ -44,7 +55,8 @@ namespace WinFormsApp1
                     CategoryID = i.CategoryID,
                     Quantity = i.Quantity,
                     Unit = i.Unit,
-                    Category = new Category { CategoryName = i.Category.CategoryName }}).ToList();
+                    Category = new Category { CategoryName = i.Category.CategoryName }
+                }).ToList();
         }
     }
 }
